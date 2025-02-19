7a77c9ef9b6eeb774b0ed3db4e02da5f20e27f45
baris <baris@heartsapiens.art>
2025-02-20 00:04:10 +0300

    Customize admin panel.

--- article/admin.py
+++ article/admin.py
@@ -1,6 +1,18 @@
+"""
+Article Admin
+"""
 from django.contrib import admin
 
 from .models import Article
 
 # Register your models here.
-admin.site.register(Article)
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    """Article Admin"""
+
+    list_display = ['title', 'author', 'created_date']
+    list_display_links = ['title', 'created_date']
+    search_fields = ['title', 'author__username']
+    list_filter = ['created_date', 'author']
+    class Meta:
+        model = Article
--- article/models.py
+++ article/models.py
@@ -1,9 +1,17 @@
+"""
+Article App Models
+"""
 from django.db import models
 
 # Create your models here.
 
 class Article(models.Model):
-    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
-    title = models.CharField(max_length=120)
-    content = models.TextField()
-    created_date = models.DateTimeField(auto_now_add=True)
+    """Article Model"""
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE, verbose_name='Yazar')
+    title = models.CharField(max_length=120, verbose_name='Başlık')
+    content = models.TextField(verbose_name='İçerik')
+    created_date = models.DateTimeField(auto_now_add=True, verbose_name='Oluşturulma Tarihi')
+
+    def __str__(self):
+        """String representation of the model"""
+        return self.title + ' (' + self.author.username  + ')'
