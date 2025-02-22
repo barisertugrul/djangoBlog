669fc015fbcaaa83f8cb0c892571a93a1914119c
baris <baris@heartsapiens.art>
2025-02-22 14:36:28 +0300

    Add article urls and context example in index view

--- article/views.py
+++ article/views.py
@@ -2,7 +2,10 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    context = {
+        'numbers': [1,2,3,4,5]
+    }
+    return render(request, 'index.html', context)
 
 def about(request):
     return render(request, 'about.html')
